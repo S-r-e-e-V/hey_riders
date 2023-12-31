a35177bca36f5bd4b0cebf9ad26fdee7ca712bc6
Sreekanth V <25sreekanth@gmail.com>
2023-12-30 21:55:10 -0500

    feat: footer updation- links and call feature

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,6 @@
 import React from "react";
-import {
-  SlSocialFacebook,
-  SlSocialTwitter,
-  SlSocialInstagram,
-  SlSocialLinkedin,
-} from "react-icons/sl";
+import { SlSocialFacebook, SlSocialInstagram } from "react-icons/sl";
+import { IoCallOutline } from "react-icons/io5";
 import "./Footer.css";
 
 const Footer = () => {
@@ -15,17 +11,17 @@ const Footer = () => {
         <a href="/services"> Our Services</a> | <a href="/contact"> Contact</a>
       </div>
       <div className="social-icons">
-        <a href="https://www.facebook.com">
+        <a
+          target="_blank"
+          href="https://www.facebook.com/profile.php?id=61551082410214&mibextid=9R9pXO"
+        >
           <SlSocialFacebook size={15} />
         </a>
-        <a href="https://www.twitter.com">
-          <SlSocialTwitter size={15} />
-        </a>
-        <a href="https://www.linkedin.com">
-          <SlSocialLinkedin size={15} />
-        </a>
-        <a href="https://www.instagram.com/heyrides">
+        <a target="_blank" href="https://www.instagram.com/heyrides">
           <SlSocialInstagram size={15} />
+        </a>
+        <a href="tel:+12263487380">
+          <IoCallOutline />
         </a>
       </div>
     </footer>
