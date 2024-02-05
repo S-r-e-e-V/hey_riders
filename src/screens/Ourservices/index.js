ac1740f86f9df0bf5accd9ef95c60b76fefe2848
Sreekanth V <25sreekanth@gmail.com>
2024-02-05 02:13:51 -0500

    feat: our services update

--- src/screens/Ourservices/index.js
+++ src/screens/Ourservices/index.js
@@ -1,32 +1,26 @@
-import React from 'react';
-import './OurServices.css'; // You should import your CSS file here
+import React from "react";
+import "./OurServices.css"; // You should import your CSS file here
 
 function OurServices() {
   return (
     <div className="background">
       <div className="title">
         <h1>Our Services</h1>
-       <div className="content">
-         <h2>Services that we offer</h2>
-        <div className="services-container">
-          <ServiceBox title="Single-Rides" />
-          <ServiceBox title="Rideshare" />
-          <ServiceBox title="Parcel Service" />
-          <ServiceBox title="Support" />
+        <div className="content">
+          <h2>Services that we offer</h2>
+          <div className="services-container">
+            <ServiceBox title="Our dedicated support team is available 24/7 to assist you with any inquiries, ensuring a reliable and seamless rideshare experience" />
+            <ServiceBox title="Convenient rideshare options from Toronto to Windsor with departure times at 09:00 am, 03:00 pm, 07:30 pm, and 10:00 pm" />
+            <ServiceBox title="Convenient rideshare options from Windsor to Toronto with departure times at 05:00 am, 10:00 am, 03:00 pm, and 06:00 pm" />
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
 }
 
 function ServiceBox({ title }) {
-  return (
-    <div className="service-box">
-      {title}
-    </div>
-  );
+  return <div className="service-box">{title}</div>;
 }
 
 export default OurServices;
-
