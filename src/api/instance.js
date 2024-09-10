7a597b410df592189a4abe724246082a0dd36aa5
Sreekanth V <25sreekanth@gmail.com>
2024-09-10 00:18:45 -0400

    updated api

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:3001";
-// const BASE_URL = "/api";
-const BASE_URL = "https://hey-rides.onrender.com";
+const BASE_URL = "/api";
+// const BASE_URL = "https://hey-rides.onrender.com";
 // const BASE_URL = "https://www.heyrides.ca/api";
 
 export default axios.create({
